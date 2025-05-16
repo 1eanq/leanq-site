b7d5c2fccea672284d5ab707ab6d29e9c7dd7e40
root <root@jammy>
2025-05-16 11:18:59 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: '#111',
+      color: '#fff',
+      fontSize: '4rem',
+      fontFamily: 'sans-serif'
+    }}>
+      LeanQ
     </div>
   );
 }
 
 export default App;
+
